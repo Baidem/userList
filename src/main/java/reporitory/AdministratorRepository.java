ce2cafd949dde8a0be34f5b4011ee6b0569d82d1
POUPAERT Vincent <baidem@gmail.com>
2022-05-25 18:33:20 +0200

    new : AdministratorRepository.java

---
+++ src/main/java/reporitory/AdministratorRepository.java
@@ -0,0 +1,15 @@
+package reporitory;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.baidem.userList.model.Administrator;
+
+@Repository
+public interface AdministratorRepository extends JpaRepository<Administrator, Long> {
+	
+	Optional<Administrator> findByName(String name);
+		
+}
