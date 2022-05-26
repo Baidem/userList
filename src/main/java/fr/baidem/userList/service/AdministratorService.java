b8805db2abd1f8b5b7a265cace5fa9a466516d01
POUPAERT Vincent <baidem@gmail.com>
2022-05-26 13:19:33 +0200

    Working for Administrator*

--- src/main/java/fr/baidem/userList/service/AdministratorService.java
+++ src/main/java/fr/baidem/userList/service/AdministratorService.java
@@ -1,5 +1,80 @@
 package fr.baidem.userList.service;
 
-public class AdministratorService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.baidem.userList.model.Administrator;
+import fr.baidem.userList.repository.AdministratorRepository;
+
+@Service
+public class AdministratorService {
+	
+	private AdministratorRepository administratorRepository;
+	
+	@Autowired
+	public AdministratorService(AdministratorRepository administratorRepository) {
+		this.administratorRepository = administratorRepository;
+	}
+	
+	public List<Administrator> findAll(){
+		return administratorRepository.findAll();
+	}
+	
+	public Administrator findByName(String name) {
+		if(name == null){ return null;	}
+		return this.administratorRepository.findByName(name);
+	}
+	
+	public boolean existId(Long id) {
+		List<Administrator> administrators=this.administratorRepository.findAll();
+		for(Administrator administrator:administrators){
+			if(administrator.getId()==id){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public Administrator getById(Long id) {
+		return administratorRepository.findById(id).orElseThrow();
+
+	}
+
+	@Transactional
+	public Administrator createAdministrator(String name, String password) {
+		if (name==null){ return null; }
+		if (this.findByName(name) != null) { return this.findByName(name); }
+		
+		Administrator administrator=new Administrator(name, password);
+		
+		administrator=this.administratorRepository.save(administrator);
+		return administrator;
+	}
+	
+	@Transactional
+	public void updateAdministrator(Long id, String name, String password) {
+		if (name != null && this.existId(id)) {
+			Administrator a=this.findByName(name);
+			if (a==null || a.getId()==id) {
+				Administrator administrator=this.getById(id);
+				administrator.setName(name);
+				administrator.setPassword(password);
+				administrator=this.administratorRepository.save(administrator);	
+			}		
+		}
+	}
+	
+	@Transactional
+	public void deleteAdministrator(Long id) {
+		Administrator administrator=this.getById(id);
+		if (administrator != null) {
+			this.administratorRepository.delete(administrator);
+		}
+	}
+	
+	
+	
 }
--- src/main/java/fr/baidem/userList/service/dto/AdministratorDTO.java
+++ src/main/java/fr/baidem/userList/service/dto/AdministratorDTO.java
@@ -1,14 +1,23 @@
 package fr.baidem.userList.service.dto;
 
+
+import fr.baidem.userList.model.Administrator;
+
 public class AdministratorDTO {
 	
 	public String name;
+	public String password;
 
 	public AdministratorDTO() {
 	}
 
 	public AdministratorDTO(String name) {
 		this.name = name;
+	}
+	
+	public AdministratorDTO(String name, String password) {
+		this(name);
+		this.password = password;
 	}
 
 	public String getName() {
@@ -18,5 +27,21 @@ public class AdministratorDTO {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+    public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public AdministratorDTO copyAdministrator(Administrator administrator){
+        
+        AdministratorDTO administratorDTO=new AdministratorDTO(administrator.getName());
+
+        return administratorDTO;
+    }
+	
 
 }
