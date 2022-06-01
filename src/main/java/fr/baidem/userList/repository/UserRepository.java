cc8d00039f348fa1974c0c9111269750edfb055d
POUPAERT Vincent <baidem@gmail.com>
2022-06-01 12:52:58 +0200

    update : UserRepository.java : add findByEnterDateAndAdminId()

--- src/main/java/fr/baidem/userList/repository/UserRepository.java
+++ src/main/java/fr/baidem/userList/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package fr.baidem.userList.repository;
 
-import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,41 +12,29 @@ public interface UserRepository  extends JpaRepository<User, Long> {
 	
 	public User getById(Long id);
 	
-	public List<User> findAll();
-	
-	public List<User> findByFirstName(String firstName);
-	
-	public List<User> findByLastName(String lastName);
-
-	public List<User> findByBirthday(Date birthday);
+	public List<User> findByAdministrator_Id(Long id);	
 	
 	public List<User> findByEmail(String email);
-	
-	public List<User> findByPhone(String Phone);
-	
-	public List<User> findByAddress(String address);
-	
-	public List<User> findByAdministrator_Id(Long id);
-	
-	@Query("select u from User u where u.administrator= :id")
-	User findTest(Long id);
-	
-	@Query("select u from User u where u.administrator= :id")
-	User findAllByAdminId(Long id);
-
+		
 	@Query("select u from User u where u.firstName= :firstName and u.administrator= :id")
-	User findByFirstNameAndAdminId(String firstName, Long id);
+	List<User> findByFirstNameAndAdminId(String firstName, Long id);
 
 	@Query("select u from User u where u.lastName= :lastName and u.administrator= :id")
-	User findByLastNameAndAdminId(String lastName, Long id);
+	List<User> findByLastNameAndAdminId(String lastName, Long id);
 
+	@Query("select u from User u where u.birthday= :birthday and u.administrator= :id")
+	List<User> findByBirthdayAndAdminId(LocalDate birthday, Long id);
+
+	@Query("select u from User u where u.enterDate= :enterDate and u.administrator= :id")
+	List<User> findByEnterDateAndAdminId(LocalDate enterDate, Long id);
+	
 	@Query("select u from User u where u.email= :email and u.administrator= :id")
-	User findByEmailAndAdminId(String email, Long id);
+	List<User> findByEmailAndAdminId(String email, Long id);
 
 	@Query("select u from User u where u.phone= :phone and u.administrator= :id")
-	User findByPhoneAndAdminId(String phone, Long id);
+	List<User> findByPhoneAndAdminId(String phone, Long id);
 	
 	@Query("select u from User u where u.address= :address and u.administrator= :id")
-	User findByAddressAndAdminId(String address, Long id);
+	List<User> findByAddressAndAdminId(String address, Long id);
 
 }
